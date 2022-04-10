fee6550770cd2e48c4e06f40774fb25a555fc40c
Ha3ap78 <102965577+Ha3ap78@users.noreply.github.com>
2022-04-10 18:17:04 +0300

    Задание 4

--- homework/task4.py
+++ homework/task4.py
@@ -1,4 +1,4 @@
 print('Для нахождения площади квадрата введите длину стороны')
 square_side = input()
-square_side = int(square_side)
+square_side = float(square_side)
 print('Площадь квадрата равна', square_side**2)
