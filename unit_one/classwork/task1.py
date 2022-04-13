ab380d133cfafaab3ee491d145ff3db1b1993baa
Ha3ap78 <na3ap78@gmail.com>
2022-04-13 23:59:44 +0300

    Мои задания

--- unit_one/classwork/task1.py
+++ unit_one/classwork/task1.py
@@ -1,14 +1,6 @@
-# todo: Дано целое число. Если оно является положительным, то прибавить к нему 1;
-# если отрицательным, то вычесть из него 2; если нулевым, то заме# нить его на 10.
-# Вывести полученное число.
+#todo: Посчитать сумму от 1 до 100
 
-print("Введите целое число")
-experimental = input()
-experimental = int(experimental)
-if experimental > 0:
-    experimental = experimental + 1
-elif experimental < 0:
-    experimental = experimental - 2
-elif experimental == 0:
-    experimental = 10
-print("Результат =", experimental)
+result = 0
+for i in range(101):
+   result = result + i
+print("Сумма равна", result)
