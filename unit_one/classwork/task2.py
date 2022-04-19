a711410341ad1dd797188dc49992df259c0eb58e
Ha3ap78 <na3ap78@gmail.com>
2022-04-19 14:34:35 +0300

    мои задания

--- unit_one/classwork/task2.py
+++ unit_one/classwork/task2.py
@@ -1,16 +1,21 @@
-#todo: Задан массив
-surname = ['Электроник','Сыроежкин', 'Чижиков', 'Кукушкина']
+#todo 2.1: Заданы множества
+# все позьзователи
+all_users = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+# пользователи не в сети
+offline_users = {'id3', 'id9', 'id7', 'id2', 'id4', 'id6'}
+print("Пользователи online: ", all_users - offline_users)
 
-# Код на выходе должен выдавать выпадающий список следующего вида.
-"""
-<select>
-	<option>Электроник</option>
-	<option>Сыроежкин</option>
-	<option>Чижиков</option>
-	<option>Кукушкина</option>
-</select>
-"""
-print("<select>")
-for i in surname:
-    print("    <option>", i, "</option>")
-print("<select>")
+#Вычислить пользователей online
+
+
+#todo 2.2: Заданы множества
+#Даны читатели книг
+readers_books = {'id3', 'id5', 'id9', 'id8', 'id2', 'id1' }
+
+#Даны читатели газет
+readers_magazines = { 'id8', 'id2', 'id1', 'id4', 'id6', 'id7', 'id10'}
+print("Пользователи читающие и книги и газеты: ", readers_books & readers_magazines)
+
+#Найти пользователей кто читает и книги и газеты
+
+
